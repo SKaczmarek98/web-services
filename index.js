129140ff52d4c2b98c687532749a13de52aaebef
SKaczmarek98 <szykacz@gmail.com>
2020-10-19 17:10:19 +0200

    zadanie 1

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const helmet = require("helmet")
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+
+app.use(helmet());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
