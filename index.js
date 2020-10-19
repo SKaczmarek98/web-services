2608847e71e36a07aca177298244138aba65fd55
SKaczmarek98 <szykacz@gmail.com>
2020-10-19 17:27:53 +0200

    Poprawione

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-app.use(helmet());
+app.use(helmet())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
 app.post('/soap', function(req, res)  {
     res.send('Hello World')
-  })
+})
 
   app.post('/rest', function(req, res)  {
     res.send('Hello World')
-  })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
